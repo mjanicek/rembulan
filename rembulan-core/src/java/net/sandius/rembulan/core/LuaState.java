0388f30224be913d4549e141839a5bede2279882
Miroslav Janíček <mira.janicek@gmail.com>
2015-10-30 21:13:28 +0100

    Adding a dummy Lua state that holds no value type metatables.

--- rembulan-core/src/java/net/sandius/rembulan/core/LuaState.java
+++ rembulan-core/src/java/net/sandius/rembulan/core/LuaState.java
@@ -10,8 +10,47 @@ public abstract class LuaState {
 	public abstract Table threadMetatable();
 	public abstract Table lightuserdataMetatable();
 
+	private static final LuaState DUMMY_STATE = new LuaState() {
+
+		@Override
+		public Table nilMetatable() {
+			return null;
+		}
+
+		@Override
+		public Table booleanMetatable() {
+			return null;
+		}
+
+		@Override
+		public Table numberMetatable() {
+			return null;
+		}
+
+		@Override
+		public Table stringMetatable() {
+			return null;
+		}
+
+		@Override
+		public Table functionMetatable() {
+			return null;
+		}
+
+		@Override
+		public Table threadMetatable() {
+			return null;
+		}
+
+		@Override
+		public Table lightuserdataMetatable() {
+			return null;
+		}
+
+	};
+
 	public static LuaState getCurrent() {
-		throw new UnsupportedOperationException();  // TODO
+		return DUMMY_STATE;
 	}
 
 }
