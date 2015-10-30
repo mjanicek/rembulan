94ce7fa2bd294dd976695f3aa40fa450b8c5faab
Miroslav Janíček <mira.janicek@gmail.com>
2015-10-30 16:10:01 +0100

    Adding an immutable int vector implementation.

---
+++ rembulan-core/src/java/net/sandius/rembulan/util/IntVector.java
@@ -0,0 +1,45 @@
+package net.sandius.rembulan.util;
+
+import java.util.Arrays;
+
+// an immutable vector of ints
+public class IntVector {
+
+	private final int[] values;
+
+	private IntVector(int[] values) {
+		Check.notNull(values);
+		this.values = values;
+	}
+
+	public static IntVector copyFrom(int[] array) {
+		Check.notNull(array);
+		int[] values = new int[array.length];
+		System.arraycopy(array, 0, values, 0, array.length);
+		return new IntVector(values);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+
+		IntVector intVector = (IntVector) o;
+
+		return Arrays.equals(values, intVector.values);
+	}
+
+	@Override
+	public int hashCode() {
+		return Arrays.hashCode(values);
+	}
+
+	public int length() {
+		return values.length;
+	}
+
+	public int get(int index) {
+		return values[index];
+	}
+
+}
