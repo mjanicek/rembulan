73ea779f96238cd03619551cb6dbac51b6c42f19
Miroslav Janíček <mira.janicek@gmail.com>
2015-10-31 14:33:58 +0100

    Adding rudimentary support for function invocation.

--- rembulan-core/src/java/net/sandius/rembulan/core/Function.java
+++ rembulan-core/src/java/net/sandius/rembulan/core/Function.java
@@ -2,6 +2,8 @@ package net.sandius.rembulan.core;
 
 public abstract class Function {
 
+	public abstract Object[] invoke(Object[] args);
+
 	@Override
 	public String toString() {
 		return "function: 0x" + Integer.toHexString(hashCode());
---
+++ rembulan-core/src/java/net/sandius/rembulan/lib/BaseLib.java
@@ -0,0 +1,68 @@
+package net.sandius.rembulan.lib;
+
+import net.sandius.rembulan.core.Function;
+import net.sandius.rembulan.core.LuaType;
+import net.sandius.rembulan.core.Value;
+import net.sandius.rembulan.util.Check;
+
+import java.io.PrintStream;
+
+public class BaseLib {
+
+	public static final Print PRINT = new Print(System.out);
+	public static final Type TYPE = new Type();
+	public static final ToString TOSTRING = new ToString();
+
+	public static class Print extends Function {
+
+		private final PrintStream ps;
+
+		public Print(PrintStream ps) {
+			Check.notNull(ps);
+			this.ps = ps;
+		}
+
+		@Override
+		public Object[] invoke(Object[] args) {
+			if (args != null) {
+				for (int i = 0; i < args.length; i++) {
+					ps.print(Value.toString(args[i]));
+					if (i + 1 < args.length) {
+						ps.print("\t");
+					}
+				}
+			}
+
+			ps.println("\n");
+
+			return null;
+		}
+
+	}
+
+	public static class Type extends Function {
+
+		@Override
+		public Object[] invoke(Object[] args) {
+			if (args == null || args.length == 0) {
+				throw new IllegalArgumentException("bad argument #1: value expected");
+			}
+
+			return new Object[] { LuaType.typeOf(args[0]).name };
+		}
+
+	}
+
+	public static class ToString extends Function {
+
+		@Override
+		public Object[] invoke(Object[] args) {
+			if (args == null || args.length == 0) {
+				throw new IllegalArgumentException("bad argument #1: value expected");
+			}
+
+			return new Object[] { Value.toString(args[0]) };
+		}
+	}
+
+}
