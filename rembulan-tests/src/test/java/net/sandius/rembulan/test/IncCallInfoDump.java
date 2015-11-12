16b3c5267acf2337b4c165d4679f532975eff0b6
Miroslav Janíček <mira.janicek@gmail.com>
2015-11-12 16:46:40 +0100

    Encapsulating constructor emitting code.

--- rembulan-tests/src/test/java/net/sandius/rembulan/test/IncCallInfoDump.java
+++ rembulan-tests/src/test/java/net/sandius/rembulan/test/IncCallInfoDump.java
@@ -30,30 +30,8 @@ public class IncCallInfoDump implements Opcodes {
 
 		cw.visitSource("inc.lua", null);
 
-		{
-			mv = cw.visitMethod(ACC_PUBLIC, "<init>", "(Lnet/sandius/rembulan/core/PreemptionContext;Lnet/sandius/rembulan/core/ObjectStack;I)V", null, null);
-			mv.visitCode();
-			Label l0 = new Label();
-			mv.visitLabel(l0);
-			mv.visitLineNumber(10, l0);
-			mv.visitVarInsn(ALOAD, 0);
-			mv.visitVarInsn(ALOAD, 1);
-			mv.visitVarInsn(ALOAD, 2);
-			mv.visitVarInsn(ILOAD, 3);
-			mv.visitMethodInsn(INVOKESPECIAL, Type.getInternalName(LuaCallInfo.class), "<init>", "(Lnet/sandius/rembulan/core/PreemptionContext;Lnet/sandius/rembulan/core/ObjectStack;I)V", false);
-			Label l1 = new Label();
-			mv.visitLabel(l1);
-			mv.visitLineNumber(11, l1);
-			mv.visitInsn(RETURN);
-			Label l2 = new Label();
-			mv.visitLabel(l2);
-			mv.visitLocalVariable("this", thisType.getDescriptor(), null, l0, l2, 0);
-			mv.visitLocalVariable("context", Type.getDescriptor(PreemptionContext.class), null, l0, l2, 1);
-			mv.visitLocalVariable("objectStack", Type.getDescriptor(ObjectStack.class), null, l0, l2, 2);
-			mv.visitLocalVariable("base", Type.INT_TYPE.getDescriptor(), null, l0, l2, 3);
-			mv.visitMaxs(4, 4);
-			mv.visitEnd();
-		}
+		LuaBytecodeMethodVisitor.emitConstructor(cw, thisType);
+
 		{
 			LuaBytecodeMethodVisitor lmv = new LuaBytecodeMethodVisitor(cw, thisType, constants, 3, 3);
 			lmv.begin();
