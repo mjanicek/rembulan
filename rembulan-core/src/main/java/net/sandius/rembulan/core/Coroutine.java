fe22440caead39db2bcebdcd815170d375c4efb8
Miroslav Janíček <mira.janicek@gmail.com>
2015-12-12 18:41:27 +0100

    Making coroutines form a doubly-linked list, adding notes about invariants.

--- rembulan-core/src/main/java/net/sandius/rembulan/core/Coroutine.java
+++ rembulan-core/src/main/java/net/sandius/rembulan/core/Coroutine.java
@@ -1,13 +1,37 @@
 package net.sandius.rembulan.core;
 
 import net.sandius.rembulan.util.Check;
+import net.sandius.rembulan.util.Cons;
 
+/*
+ Properties:
+
+   1) For any coroutine c,
+         (c.resuming == null || c.resuming.yieldingTo == c) && (c.yieldingTo == null || c.yieldingTo.resuming == c)
+         (i.e. coroutines form a doubly-linked list)
+
+   2) if c is the currently-running coroutine, then c.resuming == null
+
+   3) if c is the main coroutine, then c.yieldingTo == null
+
+ Coroutine d can be resumed from c iff
+   c != d && d.resuming == null && d.yieldingTo == null
+
+ This means that
+   c.resuming = d
+   d.yieldingTo = c
+ */
 public class Coroutine {
 
-	public final LuaState state;
+	protected final LuaState state;
 
-	public final ObjectStack objectStack;
-	public int top;
+	// paused call stack: up-to-date only iff coroutine is not running
+	protected Cons<CallInfo> callStack;
+
+	protected final ObjectStack objectStack;
+
+	protected Coroutine yieldingTo;
+	protected Coroutine resuming;
 
 	public Coroutine(LuaState state, ObjectStack objectStack) {
 		Check.notNull(state);
