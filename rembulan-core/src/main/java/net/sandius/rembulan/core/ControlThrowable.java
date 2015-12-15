cfc0ad42b79e1dcbf983ebbeec80ffabfb883c69
Miroslav Janíček <mira.janicek@gmail.com>
2015-12-15 17:33:09 +0100

    Moving implementation into the base class.

--- rembulan-core/src/main/java/net/sandius/rembulan/core/ControlThrowable.java
+++ rembulan-core/src/main/java/net/sandius/rembulan/core/ControlThrowable.java
@@ -1,21 +1,36 @@
 package net.sandius.rembulan.core;
 
+import net.sandius.rembulan.util.Check;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.ListIterator;
 
 public abstract class ControlThrowable extends Throwable {
+
+	protected final ArrayList<CallInfo> callStack;
+
+	protected ControlThrowable() {
+		this.callStack = new ArrayList<>();
+	}
 
 	@Override
 	public synchronized Throwable fillInStackTrace() {
 		return this;
 	}
 
-	public abstract void push(CallInfo ci);
+	public void push(CallInfo ci) {
+		Check.notNull(ci);
+		callStack.add(ci);
+	}
 
 	public void pushCall(Function function, int base, int ret, int pc, int numResults, int flags) {
 		push(new CallInfo(function, base, ret, pc, numResults, flags));
 	}
 
 	// LIFO iterator
-	public abstract ListIterator<CallInfo> frameIterator();
+	public ListIterator<CallInfo> frameIterator() {
+		return Collections.unmodifiableList(callStack).listIterator();
+	}
 
 }
--- rembulan-core/src/main/java/net/sandius/rembulan/core/Preempted.java
+++ rembulan-core/src/main/java/net/sandius/rembulan/core/Preempted.java
@@ -1,32 +1,5 @@
 package net.sandius.rembulan.core;
 
-import net.sandius.rembulan.util.Check;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.ListIterator;
-
 public class Preempted extends ControlThrowable {
-
-	private final ArrayList<CallInfo> callStack;
-
-	public Preempted() {
-		callStack = new ArrayList<>();
-	}
-
-	public static Preempted newInstance() {
-		return new Preempted();
-	}
-
-	@Override
-	public void push(CallInfo ci) {
-		Check.notNull(ci);
-		callStack.add(ci);
-	}
-
-	@Override
-	public ListIterator<CallInfo> frameIterator() {
-		return Collections.unmodifiableList(callStack).listIterator();
-	}
 
 }
