a16499bea0573e8fba4dd58f26e42ce12a36a165
Miroslav Janíček <mira.janicek@gmail.com>
2015-12-20 12:02:36 +0100

    Moving dispatch methods to a separate class.

--- rembulan-benchmarks/src/main/java/net/sandius/rembulan/CallReturnBenchmark.java
+++ rembulan-benchmarks/src/main/java/net/sandius/rembulan/CallReturnBenchmark.java
@@ -239,120 +239,6 @@ public class CallReturnBenchmark {
 
 	}
 
-	public static Func callTarget(LuaState state, Object target) {
-		if (target instanceof Func) {
-			return (Func) target;
-		}
-		else {
-			Object handler = Metatables.getMetamethod(state, Metatables.MT_CALL, target);
-
-			if (handler instanceof Func) {
-				return (Func) handler;
-			}
-			else {
-				throw new IllegalOperationAttemptException("call", LuaType.typeOf(target).name);
-			}
-		}
-	}
-
-	public static void mt_invoke(LuaState state, ObjectSink result, Object target) throws ControlThrowable {
-		Func fn = callTarget(state, target);
-		if (fn == target) fn.invoke(state, result);
-		else fn.invoke(state, result, target);
-	}
-
-	public static void mt_invoke(LuaState state, ObjectSink result, Object target, Object arg1) throws ControlThrowable {
-		Func fn = callTarget(state, target);
-		if (fn == target) fn.invoke(state, result, arg1);
-		else fn.invoke(state, result, target, arg1);
-	}
-
-	public static void mt_invoke(LuaState state, ObjectSink result, Object target, Object arg1, Object arg2) throws ControlThrowable {
-		Func fn = callTarget(state, target);
-		if (fn == target) fn.invoke(state, result, arg1, arg2);
-		else fn.invoke(state, result, target, arg1, arg2);
-	}
-
-	public static void mt_invoke(LuaState state, ObjectSink result, Object target, Object arg1, Object arg2, Object arg3) throws ControlThrowable {
-		Func fn = callTarget(state, target);
-		if (fn == target) fn.invoke(state, result, arg1, arg2, arg3);
-		else fn.invoke(state, result, target, arg1, arg2, arg3);
-	}
-
-	public static void mt_invoke(LuaState state, ObjectSink result, Object target, Object arg1, Object arg2, Object arg3, Object arg4) throws ControlThrowable {
-		Func fn = callTarget(state, target);
-		if (fn == target) fn.invoke(state, result, arg1, arg2, arg3, arg4);
-		else fn.invoke(state, result, target, arg1, arg2, arg3, arg4);
-	}
-
-	public static void mt_invoke(LuaState state, ObjectSink result, Object target, Object arg1, Object arg2, Object arg3, Object arg4, Object arg5) throws ControlThrowable {
-		Func fn = callTarget(state, target);
-		if (fn == target) fn.invoke(state, result, arg1, arg2, arg3, arg4, arg5);
-		else fn.invoke(state, result, new Object[] { target, arg1, arg2, arg3, arg4, arg5 });
-	}
-
-	public static void mt_invoke(LuaState state, ObjectSink result, Object target, Object[] args) throws ControlThrowable {
-		Func fn = callTarget(state, target);
-		if (fn == target) {
-			fn.invoke(state, result, args);
-		}
-		else {
-			Object[] mtArgs = new Object[args.length + 1];
-			mtArgs[0] = target;
-			System.arraycopy(args, 0, mtArgs, 1, args.length);
-			fn.invoke(state, result, mtArgs);
-		}
-	}
-
-	public static void evaluateTailCalls(LuaState state, ObjectSink r) throws ControlThrowable {
-		while (r.isTailCall()) {
-			switch (r.size()) {
-				case 0: throw new IllegalStateException();
-				case 1: mt_invoke(state, r, r._0()); break;
-				case 2: mt_invoke(state, r, r._0(), r._1()); break;
-				case 3: mt_invoke(state, r, r._0(), r._1(), r._2()); break;
-				case 4: mt_invoke(state, r, r._0(), r._1(), r._2(), r._3()); break;
-				case 5: mt_invoke(state, r, r._0(), r._1(), r._2(), r._3(), r._4()); break;
-				default: mt_invoke(state, r, r._0(), r.tailAsArray()); break;
-			}
-		}
-	}
-
-	public static void call(LuaState state, ObjectSink result, Object target) throws ControlThrowable {
-		mt_invoke(state, result, target);
-		evaluateTailCalls(state, result);
-	}
-
-	public static void call(LuaState state, ObjectSink result, Object target, Object arg1) throws ControlThrowable {
-		mt_invoke(state, result, target, arg1);
-		evaluateTailCalls(state, result);
-	}
-
-	public static void call(LuaState state, ObjectSink result, Object target, Object arg1, Object arg2) throws ControlThrowable {
-		mt_invoke(state, result, target, arg1, arg2);
-		evaluateTailCalls(state, result);
-	}
-
-	public static void call(LuaState state, ObjectSink result, Object target, Object arg1, Object arg2, Object arg3) throws ControlThrowable {
-		mt_invoke(state, result, target, arg1, arg2, arg3);
-		evaluateTailCalls(state, result);
-	}
-
-	public static void call(LuaState state, ObjectSink result, Object target, Object arg1, Object arg2, Object arg3, Object arg4) throws ControlThrowable {
-		mt_invoke(state, result, target, arg1, arg2, arg3, arg4);
-		evaluateTailCalls(state, result);
-	}
-
-	public static void call(LuaState state, ObjectSink result, Object target, Object arg1, Object arg2, Object arg3, Object arg4, Object arg5) throws ControlThrowable {
-		mt_invoke(state, result, target, arg1, arg2, arg3, arg4, arg5);
-		evaluateTailCalls(state, result);
-	}
-
-	public static void call(LuaState state, ObjectSink result, Object target, Object[] args) throws ControlThrowable {
-		mt_invoke(state, result, target, args);
-		evaluateTailCalls(state, result);
-	}
-
 	public static class RecursiveCallFunc extends AbstractFunc2 {
 
 		private final Long n;
@@ -367,7 +253,7 @@ public class CallReturnBenchmark {
 				case 1:
 					long l = ((Number) r_1).longValue();
 					if (l > 0) {
-						call(state, result, r_0, r_0, l - 1);
+						Dispatch.call(state, result, r_0, r_0, l - 1);
 						Number m = (Number) result._0();
 						result.setTo(m.longValue() + 1);
 					}
@@ -471,7 +357,7 @@ public class CallReturnBenchmark {
 	public void _4_2_recursiveCall(DummyLuaState luaState) throws ControlThrowable {
 		Func f = new RecursiveCallFunc(100);
 		ObjectSink result = newSink();
-		call(luaState, result, f, f, 20);
+		Dispatch.call(luaState, result, f, f, 20);
 		assertEquals(result._0(), 120L);
 	}
 
@@ -479,7 +365,7 @@ public class CallReturnBenchmark {
 	public void _4_3_tailCall(DummyLuaState luaState) throws ControlThrowable {
 		Func f = new TailCallFunc(100);
 		ObjectSink result = newSink();
-		call(luaState, result, f, f, 20, 0);
+		Dispatch.call(luaState, result, f, f, 20, 0);
 		assertEquals(result._0(), 120L);
 	}
 
@@ -487,7 +373,7 @@ public class CallReturnBenchmark {
 	public void _4_4_selfRecursiveTailCall(DummyLuaState luaState) throws ControlThrowable {
 		Func f = new SelfRecursiveTailCallFunc(100);
 		ObjectSink result = newSink();
-		call(luaState, result, f, 20, 0);
+		Dispatch.call(luaState, result, f, 20, 0);
 		assertEquals(result._0(), 120L);
 	}
 
---
+++ rembulan-benchmarks/src/main/java/net/sandius/rembulan/Dispatch.java
@@ -0,0 +1,130 @@
+package net.sandius.rembulan;
+
+import net.sandius.rembulan.core.ControlThrowable;
+import net.sandius.rembulan.core.IllegalOperationAttemptException;
+import net.sandius.rembulan.core.LuaState;
+import net.sandius.rembulan.core.LuaType;
+import net.sandius.rembulan.core.Metatables;
+import net.sandius.rembulan.util.ObjectSink;
+
+public abstract class Dispatch {
+
+	private Dispatch() {
+		// not to be instantiated or extended
+	}
+
+	public static Func callTarget(LuaState state, Object target) {
+		if (target instanceof Func) {
+			return (Func) target;
+		}
+		else {
+			Object handler = Metatables.getMetamethod(state, Metatables.MT_CALL, target);
+
+			if (handler instanceof Func) {
+				return (Func) handler;
+			}
+			else {
+				throw new IllegalOperationAttemptException("call", LuaType.typeOf(target).name);
+			}
+		}
+	}
+
+	public static void mt_invoke(LuaState state, ObjectSink result, Object target) throws ControlThrowable {
+		Func fn = callTarget(state, target);
+		if (fn == target) fn.invoke(state, result);
+		else fn.invoke(state, result, target);
+	}
+
+	public static void mt_invoke(LuaState state, ObjectSink result, Object target, Object arg1) throws ControlThrowable {
+		Func fn = callTarget(state, target);
+		if (fn == target) fn.invoke(state, result, arg1);
+		else fn.invoke(state, result, target, arg1);
+	}
+
+	public static void mt_invoke(LuaState state, ObjectSink result, Object target, Object arg1, Object arg2) throws ControlThrowable {
+		Func fn = callTarget(state, target);
+		if (fn == target) fn.invoke(state, result, arg1, arg2);
+		else fn.invoke(state, result, target, arg1, arg2);
+	}
+
+	public static void mt_invoke(LuaState state, ObjectSink result, Object target, Object arg1, Object arg2, Object arg3) throws ControlThrowable {
+		Func fn = callTarget(state, target);
+		if (fn == target) fn.invoke(state, result, arg1, arg2, arg3);
+		else fn.invoke(state, result, target, arg1, arg2, arg3);
+	}
+
+	public static void mt_invoke(LuaState state, ObjectSink result, Object target, Object arg1, Object arg2, Object arg3, Object arg4) throws ControlThrowable {
+		Func fn = callTarget(state, target);
+		if (fn == target) fn.invoke(state, result, arg1, arg2, arg3, arg4);
+		else fn.invoke(state, result, target, arg1, arg2, arg3, arg4);
+	}
+
+	public static void mt_invoke(LuaState state, ObjectSink result, Object target, Object arg1, Object arg2, Object arg3, Object arg4, Object arg5) throws ControlThrowable {
+		Func fn = callTarget(state, target);
+		if (fn == target) fn.invoke(state, result, arg1, arg2, arg3, arg4, arg5);
+		else fn.invoke(state, result, new Object[] { target, arg1, arg2, arg3, arg4, arg5 });
+	}
+
+	public static void mt_invoke(LuaState state, ObjectSink result, Object target, Object[] args) throws ControlThrowable {
+		Func fn = callTarget(state, target);
+		if (fn == target) {
+			fn.invoke(state, result, args);
+		}
+		else {
+			Object[] mtArgs = new Object[args.length + 1];
+			mtArgs[0] = target;
+			System.arraycopy(args, 0, mtArgs, 1, args.length);
+			fn.invoke(state, result, mtArgs);
+		}
+	}
+
+	public static void evaluateTailCalls(LuaState state, ObjectSink r) throws ControlThrowable {
+		while (r.isTailCall()) {
+			switch (r.size()) {
+				case 0: throw new IllegalStateException();
+				case 1: mt_invoke(state, r, r._0()); break;
+				case 2: mt_invoke(state, r, r._0(), r._1()); break;
+				case 3: mt_invoke(state, r, r._0(), r._1(), r._2()); break;
+				case 4: mt_invoke(state, r, r._0(), r._1(), r._2(), r._3()); break;
+				case 5: mt_invoke(state, r, r._0(), r._1(), r._2(), r._3(), r._4()); break;
+				default: mt_invoke(state, r, r._0(), r.tailAsArray()); break;
+			}
+		}
+	}
+
+	public static void call(LuaState state, ObjectSink result, Object target) throws ControlThrowable {
+		mt_invoke(state, result, target);
+		evaluateTailCalls(state, result);
+	}
+
+	public static void call(LuaState state, ObjectSink result, Object target, Object arg1) throws ControlThrowable {
+		mt_invoke(state, result, target, arg1);
+		evaluateTailCalls(state, result);
+	}
+
+	public static void call(LuaState state, ObjectSink result, Object target, Object arg1, Object arg2) throws ControlThrowable {
+		mt_invoke(state, result, target, arg1, arg2);
+		evaluateTailCalls(state, result);
+	}
+
+	public static void call(LuaState state, ObjectSink result, Object target, Object arg1, Object arg2, Object arg3) throws ControlThrowable {
+		mt_invoke(state, result, target, arg1, arg2, arg3);
+		evaluateTailCalls(state, result);
+	}
+
+	public static void call(LuaState state, ObjectSink result, Object target, Object arg1, Object arg2, Object arg3, Object arg4) throws ControlThrowable {
+		mt_invoke(state, result, target, arg1, arg2, arg3, arg4);
+		evaluateTailCalls(state, result);
+	}
+
+	public static void call(LuaState state, ObjectSink result, Object target, Object arg1, Object arg2, Object arg3, Object arg4, Object arg5) throws ControlThrowable {
+		mt_invoke(state, result, target, arg1, arg2, arg3, arg4, arg5);
+		evaluateTailCalls(state, result);
+	}
+
+	public static void call(LuaState state, ObjectSink result, Object target, Object[] args) throws ControlThrowable {
+		mt_invoke(state, result, target, args);
+		evaluateTailCalls(state, result);
+	}
+
+}
