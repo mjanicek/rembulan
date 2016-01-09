bb28d06042c5114980692a69289f77942604979b
Miroslav Janíček <mira.janicek@gmail.com>
2016-01-09 17:54:40 +0100

    Adding a spec for prototype loading and writing.

---
+++ rembulan-tests/src/test/scala/net/sandius/rembulan/test/LoaderWriterSpec.scala
@@ -0,0 +1,98 @@
+package net.sandius.rembulan.test
+
+import java.io.{ByteArrayOutputStream, ByteArrayInputStream}
+import java.nio.ByteOrder
+
+import net.sandius.rembulan.core._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{FunSpec, MustMatchers}
+
+@RunWith(classOf[JUnitRunner])
+class LoaderWriterSpec extends FunSpec with MustMatchers {
+
+  val luacPath = "luac53"
+  val luacLoader = new LuaCPrototypeLoader(luacPath)
+
+  def escape(raw: String) = PrettyPrinter.escapedString(raw)
+
+  def compile(program: String): Prototype = {
+    val visitor = new PrototypeBuilderVisitor
+    luacLoader.accept(program, visitor)
+    visitor.get
+  }
+
+  def load(bytes: Array[Byte]): Prototype = {
+    val visitor = new PrototypeBuilderVisitor()
+
+    val bais = new ByteArrayInputStream(bytes)
+    val loader = PrototypeLoader.fromInputStream(bais)
+
+    loader.accept(visitor)
+    visitor.get
+  }
+
+  def dump(proto: Prototype): Array[Byte] = {
+    val baos = new ByteArrayOutputStream()
+    val writer = new PrototypeWriter(baos, ByteOrder.LITTLE_ENDIAN, 4, 4, 4, 8, 8)
+    proto.accept(writer)
+    baos.toByteArray
+  }
+
+  def prog(program: String) {
+
+    describe("Program \"" + escape(program) + "\"") {
+
+      lazy val prototype = compile(program)
+      lazy val dumped = dump(prototype)
+      lazy val reloaded = load(dumped)
+
+      it("is accepted") {
+        prototype must not be null
+      }
+
+      it("can be dumped") {
+        dumped must not be null
+        dumped must not be empty
+      }
+
+      it("can be reloaded") {
+        reloaded must not be null
+      }
+
+      it("reloaded yields an equal prototype to the original") {
+        reloaded mustEqual prototype
+      }
+
+    }
+
+  }
+
+  prog("print( 2 )\n")
+
+  prog("print( 2 )\n" +
+       "print( 3 )\n")
+
+  prog("print( 'hello, world' )\n" +
+       "for i = 2,4 do\n" +
+       "	print( 'i', i )\n" +
+       "end\n")
+
+  prog("a=1\n" +
+       "while true do\n" +
+       "  if a>10 then\n" +
+       "     break\n" +
+       "  end\n" +
+       "  a=a+1\n" +
+       "  print( a )\n" +
+       "end\n")
+
+  prog("#!../lua\n" +
+       "print( 2 )\n")
+
+  prog("A = {g=10}\n" +
+       "print( A )\n")
+
+  prog("print( 1 == b and b )\n")
+
+}
