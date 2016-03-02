964d6a059f3cb4b6f124b41b389e06eaeee16527
Miroslav Janíček <mira.janicek@gmail.com>
2016-03-02 19:21:11 +0100

    Moving compiler logic to the ChunkCompiler class, making Chunk a simple value class holding the result.

--- rembulan-compiler/src/main/java/net/sandius/rembulan/compiler/gen/Chunk.java
+++ rembulan-compiler/src/main/java/net/sandius/rembulan/compiler/gen/Chunk.java
@@ -1,38 +1,26 @@
 package net.sandius.rembulan.compiler.gen;
 
-import net.sandius.rembulan.compiler.gen.block.AccountingNode;
-import net.sandius.rembulan.compiler.gen.block.LineInfo;
-import net.sandius.rembulan.compiler.gen.block.Linear;
-import net.sandius.rembulan.compiler.gen.block.LinearSeq;
-import net.sandius.rembulan.compiler.gen.block.LinearSeqTransformation;
-import net.sandius.rembulan.compiler.gen.block.Nodes;
 import net.sandius.rembulan.lbc.Prototype;
 import net.sandius.rembulan.util.Check;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 public class Chunk {
 
 	private final Prototype prototype;
-
-	private final ClassNameGenerator nameGenerator;
-
 	private final Map<Prototype, CompilationUnit> units;
-	private final CompilationContext ctx;
-
 	private final List<CompiledClass> classes;
 
-	public Chunk(Prototype prototype, ClassNameGenerator nameGenerator) {
+	public Chunk(Prototype prototype, Map<Prototype, CompilationUnit> units, List<CompiledClass> classes) {
 		this.prototype = Check.notNull(prototype);
-		this.nameGenerator = Check.notNull(nameGenerator);
-		this.units = new HashMap<>();
-		this.ctx = new CompilationContext(units);
-		this.classes = new LinkedList<>();
+		this.units = Collections.unmodifiableMap(Check.notNull(units));
+		this.classes = Collections.unmodifiableList(Check.notNull(classes));
+	}
+
+	public Prototype prototype() {
+		return prototype;
 	}
 
 	public Iterable<CompilationUnit> units() {
@@ -43,144 +31,6 @@ public class Chunk {
 	// preceded by their dependencies (i.e. it's post-order).
 	public Iterable<CompiledClass> classes() {
 		return Collections.unmodifiableList(classes);
-	}
-
-	@Deprecated
-	public void go() {
-		List<CompilationUnit> us = new LinkedList<>();
-
-		addUnits(prototype, us, nameGenerator);
-
-		for (CompilationUnit u : us) {
-			System.out.println("Processing " + u.name() + "...");
-			processGeneric(u);
-
-			CompiledClass ccl = u.toCompiledClass();
-			if (ccl != null) {
-				classes.add(ccl);
-			}
-			else {
-				System.out.println("No compiled bytecode for class " + u.name());
-			}
-		}
-	}
-
-	private void addUnits(Prototype prototype, List<CompilationUnit> us, ClassNameGenerator nameGen) {
-		if (!units.containsKey(prototype)) {
-			CompilationUnit u = initUnit(prototype, nameGen.className());
-			units.put(prototype, u);
-			us.add(0, u);  // prepending in order to ensure that nested prototypes are processed before their parent
-
-			for (int i = 0; i < prototype.getNestedPrototypes().size(); i++) {
-				Prototype np = prototype.getNestedPrototypes().get(i);
-				addUnits(np, us, nameGen.child(i));
-			}
-		}
-	}
-
-	private CompilationUnit initUnit(Prototype prototype, String name) {
-		CompilationUnit compilationUnit = new CompilationUnit(prototype, name, ctx);
-		compilationUnit.initGeneric();
-		return compilationUnit;
-	}
-
-	private void processGeneric(CompilationUnit compilationUnit) {
-		CompiledPrototype cp = compilationUnit.generic();
-
-		cp.insertHooks();
-
-		cp.inlineInnerJumps();
-		cp.makeBlocks();
-
-		Nodes.applyTransformation(cp.callEntry, new CollectCPUAccounting());
-
-		// remove repeated line info nodes
-		Nodes.applyTransformation(cp.callEntry, new RemoveRedundantLineNodes());
-
-		// dissolve blocks
-		cp.dissolveBlocks();
-
-		// remove all line info nodes
-//		applyTransformation(entryPoints, new LinearSeqTransformation.Remove(Predicates.isClass(LineInfo.class)));
-
-//		System.out.println();
-//		printNodes(entryPoints);
-
-		cp.updateDataFlow();
-
-		cp.inlineBranches();
-
-		// add capture nodes
-		cp.insertCaptureNodes();
-
-//		addResumptionPoints();
-
-		cp.makeBlocks();
-
-		cp.updateDataFlow();
-
-		cp.computeReturnType();
-	}
-
-	private static class CollectCPUAccounting extends LinearSeqTransformation {
-
-		@Override
-		public void apply(LinearSeq seq) {
-			List<AccountingNode> toBeRemoved = new ArrayList<>();
-
-			int cost = 0;
-
-			for (Linear n : seq.nodes()) {
-				if (n instanceof AccountingNode) {
-					AccountingNode an = (AccountingNode) n;
-					if (n instanceof AccountingNode.TickBefore) {
-						cost += 1;
-						toBeRemoved.add(an);
-					}
-					else if (n instanceof AccountingNode.Add) {
-						cost += ((AccountingNode.Add) n).cost;
-						toBeRemoved.add(an);
-					}
-				}
-			}
-
-			for (AccountingNode an : toBeRemoved) {
-				// remove all nodes
-				an.remove();
-			}
-
-			if (cost > 0) {
-				// insert cost node at the beginning
-				seq.insertAtBeginning(new AccountingNode.Add(cost));
-			}
-		}
-
-	}
-
-	private static class RemoveRedundantLineNodes extends LinearSeqTransformation {
-
-		@Override
-		public void apply(LinearSeq seq) {
-			int line = -1;
-			List<Linear> toBeRemoved = new ArrayList<>();
-
-			for (Linear n : seq.nodes()) {
-				if (n instanceof LineInfo) {
-					LineInfo lineInfoNode = (LineInfo) n;
-					if (lineInfoNode.line == line) {
-						// no need to keep this one
-						toBeRemoved.add(lineInfoNode);
-					}
-					line = lineInfoNode.line;
-				}
-			}
-
-			for (Linear n : toBeRemoved) {
-				n.remove();
-			}
-
-		}
-
 	}
 
 }
--- rembulan-compiler/src/main/java/net/sandius/rembulan/compiler/gen/ChunkCompiler.java
+++ rembulan-compiler/src/main/java/net/sandius/rembulan/compiler/gen/ChunkCompiler.java
@@ -1,13 +1,158 @@
 package net.sandius.rembulan.compiler.gen;
 
+import net.sandius.rembulan.compiler.gen.block.AccountingNode;
+import net.sandius.rembulan.compiler.gen.block.LineInfo;
+import net.sandius.rembulan.compiler.gen.block.Linear;
+import net.sandius.rembulan.compiler.gen.block.LinearSeq;
+import net.sandius.rembulan.compiler.gen.block.LinearSeqTransformation;
+import net.sandius.rembulan.compiler.gen.block.Nodes;
 import net.sandius.rembulan.lbc.Prototype;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class ChunkCompiler {
 
 	public Chunk compile(Prototype prototype, ClassNameGenerator nameGenerator) {
-		Chunk chunk = new Chunk(prototype, nameGenerator);
-		chunk.go();
-		return chunk;
+		Map<Prototype, CompilationUnit> units = new HashMap<>();
+		List<CompilationUnit> us = new LinkedList<>();
+		List<CompiledClass> classes = new LinkedList<>();
+
+		CompilationContext ctx = new CompilationContext(units);
+
+		addUnits(units, ctx, prototype, us, nameGenerator);
+
+		for (CompilationUnit u : us) {
+			System.out.println("Processing " + u.name() + "...");
+			processGeneric(u);
+
+			CompiledClass ccl = u.toCompiledClass();
+			if (ccl != null) {
+				classes.add(ccl);
+			}
+			else {
+				System.out.println("No compiled bytecode for class " + u.name());
+			}
+		}
+
+		return new Chunk(prototype, units, classes);
+	}
+
+	private void addUnits(Map<Prototype, CompilationUnit> units, CompilationContext ctx, Prototype prototype, List<CompilationUnit> us, ClassNameGenerator nameGen) {
+		if (!units.containsKey(prototype)) {
+			CompilationUnit u = new CompilationUnit(prototype, nameGen.className(), ctx);
+			u.initGeneric();
+
+			units.put(prototype, u);
+			us.add(0, u);  // prepending in order to ensure that nested prototypes are processed before their parent
+
+			for (int i = 0; i < prototype.getNestedPrototypes().size(); i++) {
+				Prototype np = prototype.getNestedPrototypes().get(i);
+				addUnits(units, ctx, np, us, nameGen.child(i));
+			}
+		}
+	}
+
+	private void processGeneric(CompilationUnit compilationUnit) {
+		CompiledPrototype cp = compilationUnit.generic();
+
+		cp.insertHooks();
+
+		cp.inlineInnerJumps();
+		cp.makeBlocks();
+
+		Nodes.applyTransformation(cp.callEntry, new CollectCPUAccounting());
+
+		// remove repeated line info nodes
+		Nodes.applyTransformation(cp.callEntry, new RemoveRedundantLineNodes());
+
+		// dissolve blocks
+		cp.dissolveBlocks();
+
+		// remove all line info nodes
+//		applyTransformation(entryPoints, new LinearSeqTransformation.Remove(Predicates.isClass(LineInfo.class)));
+
+//		System.out.println();
+//		printNodes(entryPoints);
+
+		cp.updateDataFlow();
+
+		cp.inlineBranches();
+
+		// add capture nodes
+		cp.insertCaptureNodes();
+
+//		addResumptionPoints();
+
+		cp.makeBlocks();
+
+		cp.updateDataFlow();
+
+		cp.computeReturnType();
+	}
+
+	private static class CollectCPUAccounting extends LinearSeqTransformation {
+
+		@Override
+		public void apply(LinearSeq seq) {
+			List<AccountingNode> toBeRemoved = new ArrayList<>();
+
+			int cost = 0;
+
+			for (Linear n : seq.nodes()) {
+				if (n instanceof AccountingNode) {
+					AccountingNode an = (AccountingNode) n;
+					if (n instanceof AccountingNode.TickBefore) {
+						cost += 1;
+						toBeRemoved.add(an);
+					}
+					else if (n instanceof AccountingNode.Add) {
+						cost += ((AccountingNode.Add) n).cost;
+						toBeRemoved.add(an);
+					}
+				}
+			}
+
+			for (AccountingNode an : toBeRemoved) {
+				// remove all nodes
+				an.remove();
+			}
+
+			if (cost > 0) {
+				// insert cost node at the beginning
+				seq.insertAtBeginning(new AccountingNode.Add(cost));
+			}
+		}
+
+	}
+
+	private static class RemoveRedundantLineNodes extends LinearSeqTransformation {
+
+		@Override
+		public void apply(LinearSeq seq) {
+			int line = -1;
+			List<Linear> toBeRemoved = new ArrayList<>();
+
+			for (Linear n : seq.nodes()) {
+				if (n instanceof LineInfo) {
+					LineInfo lineInfoNode = (LineInfo) n;
+					if (lineInfoNode.line == line) {
+						// no need to keep this one
+						toBeRemoved.add(lineInfoNode);
+					}
+					line = lineInfoNode.line;
+				}
+			}
+
+			for (Linear n : toBeRemoved) {
+				n.remove();
+			}
+
+		}
+
 	}
 
 }
