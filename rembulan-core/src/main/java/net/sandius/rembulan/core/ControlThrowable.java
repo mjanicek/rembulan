c9e46072e6cf9942ef98b6bde104efeabbf168a4
Miroslav Janíček <mira.janicek@gmail.com>
2016-04-19 18:49:45 +0200

    Saving execution state to control throwables, and exposing the call stack; adding a resume method to the ResumeInfo class.

--- rembulan-core/src/main/java/net/sandius/rembulan/core/ControlThrowable.java
+++ rembulan-core/src/main/java/net/sandius/rembulan/core/ControlThrowable.java
@@ -3,6 +3,7 @@ package net.sandius.rembulan.core;
 import net.sandius.rembulan.util.Check;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.ListIterator;
@@ -10,9 +11,11 @@ import java.util.ListIterator;
 public abstract class ControlThrowable extends Throwable {
 
 	protected final LinkedList<CallInfo> callStack;
+	protected final ArrayList<ResumeInfo> resumeStack;
 
 	protected ControlThrowable() {
 		this.callStack = new LinkedList<>();
+		this.resumeStack = new ArrayList<>();
 	}
 
 	@Override
@@ -20,22 +23,23 @@ public abstract class ControlThrowable extends Throwable {
 		return this;
 	}
 
+	@Deprecated
 	public void push(CallInfo ci) {
 		Check.notNull(ci);
 		callStack.addFirst(ci);
 	}
 
 	public void push(Resumable resumable, Serializable suspendedState) {
-		throw new UnsupportedOperationException();  // TODO
-	}
-
-	public void pushCall(Invokable function, int base, int ret, int pc, int numResults, int flags) {
-		push(new CallInfo(function, base, ret, pc, numResults, flags));
+		resumeStack.add(0, new ResumeInfo(resumable, suspendedState));
 	}
 
 	// LIFO iterator
 	public ListIterator<CallInfo> frameIterator() {
 		return Collections.unmodifiableList(callStack).listIterator();
+	}
+
+	public ArrayList<ResumeInfo> resumeStack() {
+		return resumeStack;
 	}
 
 }
--- rembulan-core/src/main/java/net/sandius/rembulan/core/ResumeInfo.java
+++ rembulan-core/src/main/java/net/sandius/rembulan/core/ResumeInfo.java
@@ -2,14 +2,20 @@ package net.sandius.rembulan.core;
 
 import net.sandius.rembulan.util.Check;
 
+import java.io.Serializable;
+
 public class ResumeInfo {
 
-	public final Resumable function;
-	public final Object savedState;
+	public final Resumable resumable;
+	public final Serializable savedState;
 
-	public ResumeInfo(Resumable function, Object savedState) {
-		this.function = function;
+	public ResumeInfo(Resumable resumable, Serializable savedState) {
+		this.resumable = Check.notNull(resumable);
 		this.savedState = savedState;
+	}
+
+	public void resume(LuaState state, ObjectSink result) throws ControlThrowable {
+		resumable.resume(state, result, savedState);
 	}
 
 }
