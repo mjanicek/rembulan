800c357b1a0f9923f85738dab6fab9d440da7ba5
Miroslav Janíček <mira.janicek@gmail.com>
2016-04-21 23:41:54 +0200

    Adding a skeleton for the default coroutine library implementation.

---
+++ rembulan-core/src/main/java/net/sandius/rembulan/lib/impl/DefaultCoroutineLib.java
@@ -0,0 +1,43 @@
+package net.sandius.rembulan.lib.impl;
+
+import net.sandius.rembulan.core.Function;
+import net.sandius.rembulan.lib.CoroutineLib;
+
+public class DefaultCoroutineLib extends CoroutineLib {
+
+	@Override
+	protected Function _create() {
+		return null;  // TODO
+	}
+
+	@Override
+	protected Function _resume() {
+		return null;  // TODO
+	}
+
+	@Override
+	protected Function _yield() {
+		return null;  // TODO
+	}
+
+	@Override
+	protected Function _isyieldable() {
+		return null;  // TODO
+	}
+
+	@Override
+	protected Function _status() {
+		return null;  // TODO
+	}
+
+	@Override
+	protected Function _running() {
+		return null;  // TODO
+	}
+
+	@Override
+	protected Function _wrap() {
+		return null;  // TODO
+	}
+
+}
