f8f6ad4c790cacf0e4abd1f7503bb1a18a707a10
Miroslav Janíček <mira.janicek@gmail.com>
2016-05-30 22:51:21 +0200

    Adding a stub for the default I/O library implementation.

---
+++ rembulan-core/src/main/java/net/sandius/rembulan/lib/impl/DefaultIOLib.java
@@ -0,0 +1,114 @@
+package net.sandius.rembulan.lib.impl;
+
+import net.sandius.rembulan.core.Function;
+import net.sandius.rembulan.core.Userdata;
+import net.sandius.rembulan.lib.IOLib;
+
+public class DefaultIOLib extends IOLib {
+
+	@Override
+	public Function _close() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _flush() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _input() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _lines() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _open() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _output() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _popen() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _read() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _tmpfile() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _type() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _write() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Userdata _stdin() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Userdata _stdout() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Userdata _stderr() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _file_close() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _file_flush() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _file_lines() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _file_read() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _file_seek() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _file_setvbuf() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _file_write() {
+		return null;  // TODO
+	}
+
+}
