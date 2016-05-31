dd88141c1e101ffa1c795203a9367c62e75d8c3a
Miroslav Janíček <mira.janicek@gmail.com>
2016-05-31 11:21:47 +0200

    Documenting the UpvalueFactory interface.

--- rembulan-core/src/main/java/net/sandius/rembulan/core/UpvalueFactory.java
+++ rembulan-core/src/main/java/net/sandius/rembulan/core/UpvalueFactory.java
@@ -1,7 +1,16 @@
 package net.sandius.rembulan.core;
 
+/**
+ * A factory for creating {@link Upvalue} instances.
+ */
 public interface UpvalueFactory {
 
+	/**
+	 * Creates a new Upvalue with the given initial value.
+	 *
+	 * @param initialValue  initial value, may be null
+	 * @return new instance of Upvalue with the given value
+	 */
 	Upvalue newUpvalue(Object initialValue);
 
 }
