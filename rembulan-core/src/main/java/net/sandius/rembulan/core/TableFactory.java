3230a2deaf12eff0ff5050ef36e2fc38c30081d3
Miroslav Janíček <mira.janicek@gmail.com>
2016-05-31 11:12:06 +0200

    Documenting the TableFactory interface.

--- rembulan-core/src/main/java/net/sandius/rembulan/core/TableFactory.java
+++ rembulan-core/src/main/java/net/sandius/rembulan/core/TableFactory.java
@@ -1,10 +1,26 @@
 package net.sandius.rembulan.core;
 
+/**
+ * A factory for {@link Table} instances.
+ */
 public interface TableFactory {
 
-	// equivalent to newTable(0, 0)
+	/**
+	 * Creates a new empty table. This is functionally equivalent to {@code newTable(0, 0)}.
+	 *
+	 * @return new empty table
+	 * @see #newTable(int, int)
+	 */
 	Table newTable();
 
+	/**
+	 * Creates a new empty table with the given initial capacities for its array and hash
+	 * parts.
+	 *
+	 * @param array  initial capacity for the array part
+	 * @param hash  initial capacity for the hash part
+	 * @return new empty table
+	 */
 	Table newTable(int array, int hash);
 
 }
