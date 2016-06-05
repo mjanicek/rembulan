dcfdc63d7a3ef0352bb59ff94a51e75195af6364
Miroslav Janíček <mira.janicek@gmail.com>
2016-06-05 12:16:20 +0200

    Adding stub implementations for the table and debug libraries.

---
+++ rembulan-core/src/main/java/net/sandius/rembulan/lib/impl/DefaultDebugLib.java
@@ -0,0 +1,88 @@
+package net.sandius.rembulan.lib.impl;
+
+import net.sandius.rembulan.core.Function;
+import net.sandius.rembulan.lib.DebugLib;
+
+public class DefaultDebugLib extends DebugLib {
+
+	@Override
+	public Function _debug() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _gethook() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _getinfo() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _getlocal() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _getmetatable() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _getregistry() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _getupvalue() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _getuservalue() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _sethook() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _setlocal() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _setmetatable() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _setupvalue() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _setuservalue() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _traceback() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _upvalueid() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _upvaluejoin() {
+		return null;  // TODO
+	}
+
+}
---
+++ rembulan-core/src/main/java/net/sandius/rembulan/lib/impl/DefaultTableLib.java
@@ -0,0 +1,43 @@
+package net.sandius.rembulan.lib.impl;
+
+import net.sandius.rembulan.core.Function;
+import net.sandius.rembulan.lib.TableLib;
+
+public class DefaultTableLib extends TableLib {
+
+	@Override
+	public Function _concat() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _insert() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _move() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _pack() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _remove() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _sort() {
+		return null;  // TODO
+	}
+
+	@Override
+	public Function _unpack() {
+		return null;  // TODO
+	}
+
+}
