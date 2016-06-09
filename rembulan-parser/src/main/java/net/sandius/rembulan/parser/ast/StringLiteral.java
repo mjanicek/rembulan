cb075d0c8dd12aad7c6f2c8be828e06422a510a0
Miroslav Janíček <mira.janicek@gmail.com>
2016-06-09 20:52:37 +0200

    Adding a basic implementation of StringLiteral.fromString.

--- rembulan-parser/src/main/java/net/sandius/rembulan/parser/ast/StringLiteral.java
+++ rembulan-parser/src/main/java/net/sandius/rembulan/parser/ast/StringLiteral.java
@@ -1,5 +1,7 @@
 package net.sandius.rembulan.parser.ast;
 
+import net.sandius.rembulan.util.Check;
+
 public class StringLiteral implements Literal {
 
 	private final String value;
@@ -8,12 +10,33 @@ public class StringLiteral implements Literal {
 		this.value = value;
 	}
 
+	private static String stringValueOf(String s) {
+		Check.notNull(s);
+
+		StringBuilder bld = new StringBuilder();
+
+		int i = 0;
+
+		boolean dbl = s.charAt(0) == '"';
+
+		// ignore the initial quotation mark
+		i += 1;
+
+		while (i < s.length() - 1) {
+			// TODO: escapes
+			bld.append(s.charAt(i));
+			i += 1;
+		}
+
+		return bld.toString();
+	}
+
 	public static StringLiteral fromString(String s) {
-		throw new UnsupportedOperationException();  // TODO
+		return new StringLiteral(stringValueOf(s));
 	}
 
 	public static StringLiteral fromName(Name n) {
-		throw new UnsupportedOperationException();  // TODO
+		return new StringLiteral(n.value());
 	}
 
 	public String value() {
