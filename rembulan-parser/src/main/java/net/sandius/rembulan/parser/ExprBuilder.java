45bc1502e4a3974049fea8e0950a41a9a6cee98a
Miroslav Janíček <mira.janicek@gmail.com>
2016-06-09 16:48:41 +0200

    Storing a single expression in the ExprBuilder.

--- rembulan-parser/src/main/java/net/sandius/rembulan/parser/ExprBuilder.java
+++ rembulan-parser/src/main/java/net/sandius/rembulan/parser/ExprBuilder.java
@@ -3,26 +3,49 @@ package net.sandius.rembulan.parser;
 import net.sandius.rembulan.parser.ast.BinaryOperation;
 import net.sandius.rembulan.parser.ast.Expr;
 import net.sandius.rembulan.parser.ast.UnaryOperation;
+import net.sandius.rembulan.util.Check;
 
 class ExprBuilder {
 
+	private Expr expr;
+
 	ExprBuilder() {
+		this.expr = null;
 	}
 
 	public void add(UnaryOperation op) {
-		// TODO
+		Check.notNull(op);
+		throw new UnsupportedOperationException();  // TODO
 	}
 
 	public void add(BinaryOperation op) {
-		// TODO
+		Check.notNull(op);
+		throw new UnsupportedOperationException();  // TODO
 	}
 
 	public void add(Expr expr) {
-		// TODO
+		Check.notNull(expr);
+
+		// FIXME
+
+		if (this.expr != null) {
+			throw new UnsupportedOperationException();
+		}
+		else {
+			this.expr = expr;
+		}
 	}
 
-	public static Expr build() {
-		throw new UnsupportedOperationException();  // TODO
+	public Expr build() {
+
+		// FIXME
+
+		if (expr != null) {
+			return expr;
+		}
+		else {
+			throw new UnsupportedOperationException();
+		}
 	}
 
 }
