c506fab9073173b6cb95312d437035e9883fff94
Miroslav Janíček <mira.janicek@gmail.com>
2016-06-09 17:16:08 +0200

    Adding operator precedence and associativity.

--- rembulan-parser/src/main/java/net/sandius/rembulan/parser/ast/Operator.java
+++ rembulan-parser/src/main/java/net/sandius/rembulan/parser/ast/Operator.java
@@ -2,42 +2,72 @@ package net.sandius.rembulan.parser.ast;
 
 public interface Operator {
 
+	int precedence();
+
 	enum Binary implements Operator {
 
-		ADD,
-		SUB,
-		MUL,
-		DIV,
-		MOD,
-		POW,
-		IDIV,
+		ADD    (8, true),
+		SUB    (8, true),
+		MUL    (9, true),
+		DIV    (9, true),
+		IDIV   (9, true),
+		MOD    (9, true),
+		POW   (11, false),
 
-		CONCAT,
+		CONCAT  (7, false),
 
-		BAND,
-		BOR,
-		BXOR,
-		SHL,
-		SHR,
+		BAND  (5, true),
+		BOR   (3, true),
+		BXOR  (4, true),
+		SHL   (6, true),
+		SHR   (6, true),
 
-		EQ,
-		NEQ,
-		LT,
-		LE,
-		GT,
-		GE,
+		EQ   (2, true),
+		NEQ  (2, true),
+		LT   (2, true),
+		LE   (2, true),
+		GT   (2, true),
+		GE   (2, true),
 
-		AND,
-		OR
+		AND  (1, true),
+		OR   (0, true);
+
+		private final int precedence;
+		private final boolean leftAssoc;
+
+		Binary(int precedence, boolean leftAssoc) {
+			this.precedence = precedence;
+			this.leftAssoc = leftAssoc;
+		}
+
+		@Override
+		public int precedence() {
+			return precedence;
+		}
+
+		public boolean isLeftAssociative() {
+			return leftAssoc;
+		}
 
 	}
 
 	enum Unary implements Operator {
 
-		UNM,
-		BNOT,
-		LEN,
-		NOT
+		UNM  (10),
+		BNOT (10),
+		LEN  (10),
+		NOT  (10);
+
+		private final int precedence;
+
+		Unary(int precedence) {
+			this.precedence = precedence;
+		}
+
+		@Override
+		public int precedence() {
+			return precedence;
+		}
 
 	}
 
