f4a0a205e7c635708eb1c3442c74b1a3c8d52035
Miroslav Janíček <mira.janicek@gmail.com>
2016-06-10 22:28:36 +0200

    Cleaning up the expression builder.

--- rembulan-parser/src/main/java/net/sandius/rembulan/parser/ExprBuilder.java
+++ rembulan-parser/src/main/java/net/sandius/rembulan/parser/ExprBuilder.java
@@ -35,27 +35,21 @@ class ExprBuilder {
 		Check.notNull(newOp);
 		Check.notNull(top);
 
-		if (newOp instanceof Operator.Unary) {
-			return false;
-		}
-
-		return (!isRightAssociative(newOp) && newOp.precedence() <= top.precedence())
-				|| (isRightAssociative(newOp) && newOp.precedence() < top.precedence());
+		return !(newOp instanceof Operator.Unary)
+				&& (isRightAssociative(newOp)
+						? newOp.precedence() < top.precedence()
+						: newOp.precedence() <= top.precedence());
 	}
 
 	private void makeOp(Operator op) {
 		if (op instanceof Operator.Binary) {
 			Expr r = operandStack.pop();
 			Expr l = operandStack.pop();
-			Expr e = new BinaryOperationExpr((Operator.Binary) op, l, r);
-			System.out.println("pushing expr: " + e);
-			operandStack.push(e);
+			operandStack.push(new BinaryOperationExpr((Operator.Binary) op, l, r));
 		}
 		else if (op instanceof Operator.Unary) {
 			Expr a = operandStack.pop();
-			Expr e = new UnaryOperationExpr((Operator.Unary) op, a);
-			System.out.println("pushing expr: " + e);
-			operandStack.push(e);
+			operandStack.push(new UnaryOperationExpr((Operator.Unary) op, a));
 		}
 		else {
 			throw new IllegalStateException("Illegal operator: " + op);
@@ -64,19 +58,14 @@ class ExprBuilder {
 
 	public void add(Operator op) {
 		Check.notNull(op);
-		System.out.println("adding op: " + op);
-
 		while (!operatorStack.isEmpty() && hasLesserPrecedence(op, operatorStack.peek())) {
 			makeOp(operatorStack.pop());
 		}
-
 		operatorStack.push(op);
 	}
 
 	public void add(Expr expr) {
 		Check.notNull(expr);
-		System.out.println("adding expr: " + expr);
-
 		operandStack.push(expr);
 	}
 
