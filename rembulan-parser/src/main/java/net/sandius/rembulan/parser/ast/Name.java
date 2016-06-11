a70f43cbc8c7ea3f56c166c8174d7b522a74fe48
Miroslav Janíček <mira.janicek@gmail.com>
2016-06-11 21:15:52 +0200

    Checking names for validity.

--- rembulan-parser/src/main/java/net/sandius/rembulan/parser/ast/Name.java
+++ rembulan-parser/src/main/java/net/sandius/rembulan/parser/ast/Name.java
@@ -1,7 +1,5 @@
 package net.sandius.rembulan.parser.ast;
 
-import net.sandius.rembulan.util.Check;
-
 public class Name {
 
 	private final String value;
@@ -14,19 +12,38 @@ public class Name {
 		return new Name(s);
 	}
 
+	public static boolean isValidName(String s) {
+		if (s == null) {
+			return false;
+		}
+
+		if (s.isEmpty()) {
+			return false;
+		}
+
+		char c = s.charAt(0);
+
+		if ((c < 'a' || c > 'z') && (c < 'A' || c > 'Z') && (c != '_')) return false;
+
+		for (int i = 1; i < s.length(); i++) {
+			c = s.charAt(i);
+			if ((c < 'a' || c > 'z') && (c < 'A' || c > 'Z') && (c != '_') && (c < '0' || c > '9')) return false;
+		}
+
+		return true;
+	}
+
 	public static String checkValidName(String s) {
-		Check.notNull(s);
-		// TODO
-		return s;
+		if (!isValidName(s)) {
+			throw new IllegalArgumentException("Not a valid name: " + s);
+		}
+		else {
+			return s;
+		}
 	}
 
 	public String value() {
 		return value;
-	}
-
-	@Override
-	public String toString() {
-		return "(name " + value + ")";
 	}
 
 }
