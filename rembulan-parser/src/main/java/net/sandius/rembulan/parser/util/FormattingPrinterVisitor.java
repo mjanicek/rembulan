2acb4bcf603d22eeefd7e40d8727d595192a0a4d
Miroslav Janíček <mira.janicek@gmail.com>
2016-06-12 22:07:46 +0200

    Removing unused import.

--- rembulan-parser/src/main/java/net/sandius/rembulan/parser/util/FormattingPrinterVisitor.java
+++ rembulan-parser/src/main/java/net/sandius/rembulan/parser/util/FormattingPrinterVisitor.java
@@ -6,7 +6,6 @@ import net.sandius.rembulan.util.Check;
 
 import java.io.PrintWriter;
 import java.util.Iterator;
-import java.util.List;
 
 public class FormattingPrinterVisitor implements StatementVisitor, ExprVisitor, LiteralVisitor {
 
