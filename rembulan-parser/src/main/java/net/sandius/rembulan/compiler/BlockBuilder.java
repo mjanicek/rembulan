e440a051611dbb02f9ccea9b2b1685487bbc69ce
Miroslav Janíček <mira.janicek@gmail.com>
2016-06-22 15:43:35 +0200

    Splitting instruction sequences to basic blocks.

--- rembulan-parser/src/main/java/net/sandius/rembulan/compiler/BlockBuilder.java
+++ rembulan-parser/src/main/java/net/sandius/rembulan/compiler/BlockBuilder.java
@@ -1,19 +1,25 @@
 package net.sandius.rembulan.compiler;
 
 import net.sandius.rembulan.compiler.ir.IRNode;
+import net.sandius.rembulan.compiler.ir.JmpNode;
 import net.sandius.rembulan.compiler.ir.Label;
 import net.sandius.rembulan.util.Check;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 public class BlockBuilder {
 
 	private final ArrayList<Block> blocks;
+	private final Map<Label, Integer> pending;
 
 	public BlockBuilder(Label entryLabel) {
 		this.blocks = new ArrayList<>();
+		this.pending = new HashMap<>();
+
 		blocks.add(new Block(entryLabel));
 	}
 
@@ -65,21 +71,91 @@ public class BlockBuilder {
 		};
 	}
 
-	public void add(IRNode node) {
-		Check.notNull(node);
-
+	private void appendToCurrentBlock(IRNode node) {
 		Block currentBlock = blocks.get(blocks.size() - 1);
 		currentBlock.insns.add(node);
 	}
 
+	public void add(IRNode node) {
+		Check.notNull(node);
+
+		if (node instanceof JmpNode) {
+			Label jmpDest = ((JmpNode) node).jmpDest();
+			useLabel(jmpDest);
+		}
+
+		if (node instanceof Label) {
+			defLabel((Label) node);
+		}
+		else {
+			appendToCurrentBlock(node);
+		}
+	}
+
+	private void useLabel(Label l) {
+		int bIdx = findBlock(l);
+		if (bIdx < 0) {
+			Integer n = pending.get(l);
+			if (n != null) {
+				pending.put(l, n + 1);
+			}
+			else {
+				pending.put(l, 1);
+			}
+		}
+		else {
+			ensureHead(bIdx, l);
+		}
+	}
+
+	private void defLabel(Label l) {
+		Integer uses = pending.get(l);
+		if (uses != null && uses > 1) {
+			blocks.add(new Block(l));
+		}
+		else {
+			appendToCurrentBlock(l);
+		}
+	}
+
+	private int findBlock(Label l) {
+		for (int i = 0; i < blocks.size(); i++) {
+			Block b = blocks.get(i);
+			if (b.head.equals(l) || b.insns.contains(l)) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	private void ensureHead(int bIdx, Label l) {
+		Block b = blocks.get(bIdx);
+		if (!b.head.equals(l)) {
+			int idx = b.insns.indexOf(l);
+
+			assert (idx >= 0);
+
+			ArrayList<IRNode> pred = new ArrayList<>(b.insns.subList(0, idx));
+			ArrayList<IRNode> succ = new ArrayList<>(b.insns.subList(idx + 1, b.insns.size()));
+
+			// replace blocks
+			blocks.set(bIdx, new Block(b.head, pred));
+			blocks.add(bIdx + 1, new Block(l, succ));
+		}
+	}
+
 	private static class Block {
 
-		private final Label label;
+		private final Label head;
 		private final ArrayList<IRNode> insns;
 
-		private Block(Label label) {
-			this.label = Check.notNull(label);
-			this.insns = new ArrayList<>();
+		private Block(Label head, ArrayList<IRNode> insns) {
+			this.head = Check.notNull(head);
+			this.insns = Check.notNull(insns);
+		}
+
+		private Block(Label head) {
+			this(head, new ArrayList<IRNode>());
 		}
 
 		public Iterator<IRNode> iterator() {
@@ -97,7 +173,7 @@ public class BlockBuilder {
 					if (idx < insns.size()) {
 						if (idx < 0) {
 							idx += 1;
-							return label;
+							return head;
 						}
 						else {
 							return insns.get(idx++);
