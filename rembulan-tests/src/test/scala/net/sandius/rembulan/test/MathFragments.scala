dd4519f26846b7c072167391bb2126aac3d21304
Miroslav Janíček <mira.janicek@gmail.com>
2016-07-20 18:33:48 +0200

    Correcting the expected error message.

--- rembulan-tests/src/test/scala/net/sandius/rembulan/test/MathFragments.scala
+++ rembulan-tests/src/test/scala/net/sandius/rembulan/test/MathFragments.scala
@@ -10,7 +10,7 @@ object MathFragments extends FragmentBundle with FragmentExpectations with OneLi
 
       program ("return math.random(0, 0)") succeedsWith (0)
 
-      program ("return math.random(-10000000000000000000, 10000000000000000000)") failsWith (classOf[IllegalArgumentException], "bad argument #1 to 'random' (interval is empty)")
+      program ("return math.random(-10000000000000000000, 10000000000000000000)") failsWith (classOf[IllegalArgumentException], "bad argument #1 to 'random' (number has no integer representation)")
 
       program ("math.random(1 << 63, (1 << 63) - 1)") failsWith (classOf[IllegalArgumentException], "bad argument #1 to 'random' (interval too large)")
     }
