36d4dbc573e52472020bcd3e05a84d22a6710cf2
Miroslav Janíček <mira.janicek@gmail.com>
2016-08-17 00:49:58 +0200

    Cleaning up the Arithmetic class and documenting it.

--- rembulan-core/src/main/java/net/sandius/rembulan/core/Arithmetic.java
+++ rembulan-core/src/main/java/net/sandius/rembulan/core/Arithmetic.java
@@ -25,14 +25,56 @@ import static net.sandius.rembulan.LuaMathOperators.rawpow;
 import static net.sandius.rembulan.LuaMathOperators.rawsub;
 import static net.sandius.rembulan.LuaMathOperators.rawunm;
 
+/**
+ * A representation of one of the two arithmetic modes (integer or float).
+ *
+ * <p>This class serves as a bridge between the representation of Lua numbers as
+ * {@link java.lang.Number} and the appropriate dispatch of arithmetic operations
+ * via the methods of {@link net.sandius.rembulan.LuaMathOperators}.</p>
+ *
+ * <p>There are two concrete instances of this class, {@link #FLOAT} (the float
+ * arithmetic) and {@link #INTEGER} (the integer arithmetic). In order to obtain
+ * the correct instance for the given numbers, use the static methods
+ * {@link #of(Number, Number)} (for lookup based on two arguments, i.e. for binary
+ * operations) or {@link #of(Number)} (for lookup based on a single argument, i.e.
+ * for unary minus).</p>
+ *
+ * <p>The arithmetic methods of this class yield boxed results.</p>
+ *
+ * <p><b>Example:</b> Given two objects {@code a} and {@code b} of type {@code Number},
+ *  use the {@code Arithmetic} class to compute the value of the Lua expression
+ *  {@code (a // b)}:</p>
+ *
+ * <pre>
+ *   // Number a, b
+ *   final Number result;
+ *   Arithmetic math = Arithmetic.of(a, b);
+ *   if (math != null) {
+ *       result = math.do_idiv(a, b);
+ *   }
+ *   else {
+ *       throw new IllegalStateException("a or b is nil");
+ *   }
+ * </pre>
+ */
 public abstract class Arithmetic {
 
 	private Arithmetic() {
 		// not to be instantiated by the outside world
 	}
 
-	public static final IntegerArithmetic INTEGER = new IntegerArithmetic();
-	public static final FloatArithmetic FLOAT = new FloatArithmetic();
+	/**
+	 * Integer arithmetic.
+	 *
+	 * <p>Invokes arithmetic operations in {@link net.sandius.rembulan.LuaMathOperators}
+	 * with all arguments converted to integers (i.e. {@code long}s)</p>
+	 */
+	public static final Arithmetic INTEGER = new IntegerArithmetic();
+
+	/**
+	 * Float arithmetic.
+	 */
+	public static final Arithmetic FLOAT = new FloatArithmetic();
 
 	public abstract Number do_add(Number a, Number b);
 	public abstract Number do_sub(Number a, Number b);
@@ -44,7 +86,11 @@ public abstract class Arithmetic {
 
 	public abstract Number do_unm(Number n);
 
-	public static final class IntegerArithmetic extends Arithmetic {
+	private static final class IntegerArithmetic extends Arithmetic {
+
+		private IntegerArithmetic() {
+			// not to be instantiated by the outside world
+		}
 
 		@Override
 		public Long do_add(Number a, Number b) {
@@ -88,7 +134,11 @@ public abstract class Arithmetic {
 
 	}
 
-	public static final class FloatArithmetic extends Arithmetic {
+	private static final class FloatArithmetic extends Arithmetic {
+
+		private FloatArithmetic() {
+			// not to be instantiated by the outside world
+		}
 
 		@Override
 		public Double do_add(Number a, Number b) {
@@ -132,10 +182,20 @@ public abstract class Arithmetic {
 
 	}
 
-	public static Arithmetic of(Object a, Object b) {
-		return of(Conversions.arithmeticValueOf(a), Conversions.arithmeticValueOf(b));
-	}
-
+	/**
+	 * Return an arithmetic based on the concrete types of the arguments {@code a}
+	 * and {@code b}.
+	 *
+	 * <p>If either of {@code a} or {@code b} is {@code null}, returns {@code null}.
+	 * Otherwise, if either of {@code a} or {@code b} a Lua float, returns {@link #FLOAT}.
+	 * If {@code a} and {@code b} are both Lua integers, returns {@link #INTEGER}.</p>
+	 *
+	 * @param a  first argument, may be {@code null}
+	 * @param b  second argument, may be {@code null}
+	 * @return  {@code null} if {@code a} or {@code b} is {@code null};
+	 *          {@link #FLOAT} if {@code a} or {@code b} is a Lua float;
+	 *          {@link #INTEGER} if {@code a} and {@code b} are Lua integers
+	 */
 	public static Arithmetic of(Number a, Number b) {
 		if (a == null || b == null) {
 			return null;
@@ -149,10 +209,18 @@ public abstract class Arithmetic {
 		}
 	}
 
-	public static Arithmetic of(Object o) {
-		return of(Conversions.arithmeticValueOf(o));
-	}
-
+	/**
+	 * Return an arithmetic based on the concrete type of the argument {@code n}.
+	 *
+	 * If {@code n} is {@code null}, returns {@code null}; otherwise, returns
+	 * {@link #FLOAT} if {@code n} is a Lua float, or {@link #INTEGER} if {@code n}
+	 * is a Lua integer.
+	 *
+	 * @param n  the argument, may be {@code null}
+	 * @return  {@code null} if {@code n} is {@code null};
+	 *          {@link #FLOAT} if {@code n} is a Lua float;
+	 *          {@link #INTEGER} if {@code n} is a Lua integer
+	 */
 	public static Arithmetic of(Number n) {
 		if (n == null) {
 			return null;
