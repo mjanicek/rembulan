694146d56f21a96b2fc5cbff6cb7ce2b2a60dbf5
Miroslav Janíček <mira.janicek@gmail.com>
2016-09-06 22:15:22 +0200

    Documenting Check methods.

--- rembulan-runtime/src/main/java/net/sandius/rembulan/util/Check.java
+++ rembulan-runtime/src/main/java/net/sandius/rembulan/util/Check.java
@@ -16,12 +16,25 @@
 
 package net.sandius.rembulan.util;
 
-public abstract class Check {
+/**
+ * Static methods for validating inputs.
+ */
+public final class Check {
 
 	private Check() {
 		// not to be instantiated
 	}
 
+	/**
+	 * Throws an {@link IllegalArgumentException} if the argument {@code o} is {@code null};
+	 * otherwise, returns {@code o}.
+	 *
+	 * @param o  the argument to be checked for being {@code null}
+	 * @param <T>  the type of {@code o}
+	 * @return  {@code o} if {@code o} is not {@code null}
+	 *
+	 * @throws IllegalArgumentException  if {@code o} is {@code null}
+	 */
 	public static <T> T notNull(T o) {
 		if (o == null) {
 			throw new IllegalArgumentException("argument is null");
@@ -29,12 +42,30 @@ public abstract class Check {
 		return o;
 	}
 
+	/**
+	 * Throws an {@link IllegalArgumentException} if the argument {@code o} is not {@code null}.
+	 *
+	 * @param o  the argument to be checked for being {@code null}
+	 *
+	 * @throws IllegalArgumentException  if {@code o} is not {@code null}
+	 */
 	public static void isNull(Object o) {
 		if (o != null) {
 			throw new IllegalArgumentException("argument is not null: " + o);
 		}
 	}
 
+	/**
+	 * Throws an {@link IllegalArgumentException} if {@code a != b}; otherwise, returns
+	 * {@code a}.
+	 *
+	 * @param a  the first argument, may be {@code null}
+	 * @param b  the second argument, may be {@code null}
+	 * @param <T>  the type of {@code a} and {@code b}
+	 * @return  {@code a} if {@code a == b}
+	 *
+	 * @throws IllegalArgumentException  if {@code a != b}
+	 */
 	public static <T> T isEq(T a, T b) {
 		if (a != b) {
 			throw new IllegalArgumentException("argument " + a + " is not equal to " + b);
@@ -42,6 +73,16 @@ public abstract class Check {
 		return a;
 	}
 
+	/**
+	 * Throws an {@link IllegalArgumentException} if {@code a != b}; otherwise, returns
+	 * {@code a}.
+	 *
+	 * @param a  the first argument
+	 * @param b  the second argument
+	 * @return  {@code a} if {@code a == b}
+	 *
+	 * @throws IllegalArgumentException  if {@code a != b}
+	 */
 	public static int isEq(int a, int b) {
 		if (a != b) {
 			throw new IllegalArgumentException("integer " + a + " is not equal to " + b);
@@ -49,6 +90,17 @@ public abstract class Check {
 		return a;
 	}
 
+	/**
+	 * Throws an {@link IllegalArgumentException} if the integer {@code n} is outside of
+	 * the range [{@code min}, {@code max}] (inclusive); otherwise, returns {@code n}.
+	 *
+	 * @param n  the integer to test for inclusion in the range
+	 * @param min  minimum
+	 * @param max  maximum
+	 * @return  {@code n} if {@code n >= min && n <= max}
+	 *
+	 * @throws IllegalArgumentException  if {@code n < min || n > max}
+	 */
 	public static int inRange(int n, int min, int max) {
 		if (n < min && n > max) {
 			throw new IllegalArgumentException("integer " + n + " out of range: [" + min + ", " + max + "]");
@@ -56,6 +108,16 @@ public abstract class Check {
 		return n;
 	}
 
+	/**
+	 * Throws an {@link IllegalArgumentException} if the integer {@code n} is not lesser than
+	 * {@code limit}; otherwise, returns {@code n}.
+	 *
+	 * @param n  the integer to be tested
+	 * @param limit  the limit
+	 * @return  {@code n} if {@code n < limit}
+	 *
+	 * @throws IllegalArgumentException  if {@code n >= limit}
+	 */
 	public static int lt(int n, int limit) {
 		if (!(n < limit)) {
 			throw new IllegalArgumentException("integer " + n + " is not lesser than " + limit);
@@ -63,6 +125,16 @@ public abstract class Check {
 		return n;
 	}
 
+	/**
+	 * Throws an {@link IllegalArgumentException} if the long {@code n} is not lesser than
+	 * {@code limit}; otherwise, returns {@code n}.
+	 *
+	 * @param n  the long to be tested
+	 * @param limit  the limit
+	 * @return  {@code n} if {@code n < limit}
+	 *
+	 * @throws IllegalArgumentException  if {@code n >= limit}
+	 */
 	public static long lt(long n, long limit) {
 		if (!(n < limit)) {
 			throw new IllegalArgumentException("long " + n + " is not lesser than " + limit);
@@ -70,6 +142,16 @@ public abstract class Check {
 		return n;
 	}
 
+	/**
+	 * Throws an {@link IllegalArgumentException} if the integer {@code n} is not greater than
+	 * {@code limit}; otherwise, returns {@code n}.
+	 *
+	 * @param n  the integer to be tested
+	 * @param limit  the limit
+	 * @return  {@code n} if {@code n > limit}
+	 *
+	 * @throws IllegalArgumentException  if {@code n <= limit}
+	 */
 	public static int gt(int n, int limit) {
 		if (!(n > limit)) {
 			throw new IllegalArgumentException("integer " + n + " is not greater than " + limit);
@@ -77,6 +159,16 @@ public abstract class Check {
 		return n;
 	}
 
+	/**
+	 * Throws an {@link IllegalArgumentException} if the long {@code n} is not greater than
+	 * {@code limit}; otherwise, returns {@code n}.
+	 *
+	 * @param n  the long to be tested
+	 * @param limit  the limit
+	 * @return  {@code n} if {@code n > limit}
+	 *
+	 * @throws IllegalArgumentException  if {@code n <= limit}
+	 */
 	public static long gt(long n, long limit) {
 		if (!(n > limit)) {
 			throw new IllegalArgumentException("long " + n + " is not greater than " + limit);
@@ -84,30 +176,80 @@ public abstract class Check {
 		return n;
 	}
 
+	/**
+	 * Throws an {@link IllegalArgumentException} if {@code b} is {@code false}.
+	 *
+	 * @param b  boolean value to test
+	 *
+	 * @throws IllegalArgumentException  if {@code b} is {@code false}
+	 */
 	public static void isTrue(boolean b) {
 		if (!b) {
 			throw new IllegalArgumentException("condition is false");
 		}
 	}
 
+	/**
+	 * Throws an {@link IllegalArgumentException} if {@code b} is {@code true}.
+	 *
+	 * @param b  boolean value to test
+	 *
+	 * @throws IllegalArgumentException  if {@code b} is {@code true}
+	 */
 	public static void isFalse(boolean b) {
 		if (b) {
 			throw new IllegalArgumentException("condition is true");
 		}
 	}
 
+	/**
+	 * Throws an {@link IllegalArgumentException} if the integer {@code n} is negative;
+	 * otherwise, returns {@code n}.
+	 *
+	 * @param n  the integer to be tested
+	 * @return  {@code n} if {@code n >= 0}
+	 *
+	 * @throws IllegalArgumentException  if {@code n < 0}
+	 */
 	public static int nonNegative(int n) {
 		return gt(n, -1);
 	}
 
+	/**
+	 * Throws an {@link IllegalArgumentException} if the long {@code n} is negative;
+	 * otherwise, returns {@code n}.
+	 *
+	 * @param n  the long to be tested
+	 * @return  {@code n} if {@code n >= 0}
+	 *
+	 * @throws IllegalArgumentException  if {@code n < 0}
+	 */
 	public static long nonNegative(long n) {
 		return gt(n, -1);
 	}
 
+	/**
+	 * Throws an {@link IllegalArgumentException} if the integer {@code n} is not positive;
+	 * otherwise, returns {@code n}.
+	 *
+	 * @param n  the integer to be tested
+	 * @return  {@code n} if {@code n > 0}
+	 *
+	 * @throws IllegalArgumentException  if {@code n <= 0}
+	 */
 	public static int positive(int n) {
 		return gt(n, 0);
 	}
 
+	/**
+	 * Throws an {@link IllegalArgumentException} if the double {@code n} is <i>NaN</i>;
+	 * otherwise, returns {@code n}.
+	 *
+	 * @param n  the double to be tested
+	 * @return  {@code n} if is not <i>NaN</i>
+	 *
+	 * @throws IllegalArgumentException  if {@code n} is <i>NaN</i>
+	 */
 	public static double notNaN(double n) {
 		if (Double.isNaN(n)) {
 			throw new IllegalArgumentException("argument is NaN");
