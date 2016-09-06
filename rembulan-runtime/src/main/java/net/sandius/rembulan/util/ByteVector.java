b1e65c6101b1733b10b6f9b880e88927badfb071
Miroslav Janíček <mira.janicek@gmail.com>
2016-09-06 22:52:31 +0200

    Documenting ByteVector, making it final.

--- rembulan-runtime/src/main/java/net/sandius/rembulan/util/ByteVector.java
+++ rembulan-runtime/src/main/java/net/sandius/rembulan/util/ByteVector.java
@@ -17,23 +17,54 @@
 package net.sandius.rembulan.util;
 
 import java.util.Arrays;
+import java.util.Objects;
 
-public class ByteVector {
+/**
+ * A read-only array of bytes.
+ */
+public final class ByteVector {
 
 	private final byte[] bytes;
 
 	private ByteVector(byte[] bytes) {
-		this.bytes = Check.notNull(bytes);
+		this.bytes = Objects.requireNonNull(bytes);
 	}
 
+	/**
+	 * Wraps the byte array {@code bytes} into a byte vector.
+	 *
+	 * <p>Note that the byte vector will be backed by {@code bytes}, so any changes
+	 * of the contents of {@code bytes} will be reflected in the resulting byte vector.</p>
+	 *
+	 * @param bytes  the byte array, must not be {@code null}
+	 * @return  a byte vector wrapping {@code bytes}
+	 *
+	 * @throws NullPointerException  if {@code bytes} is {@code null}
+	 */
 	public static ByteVector wrap(byte[] bytes) {
 		return new ByteVector(bytes);
 	}
 
+	/**
+	 * Creates a new byte vectory from a copy of {@code bytes}.
+	 *
+	 * @param bytes  the byte array, must not be {@code null}
+	 * @return  a byte vector containing a copy of {@code bytes}
+	 *
+	 * @throws NullPointerException  if {@code bytes} is {@code null}
+	 */
 	public static ByteVector copyFrom(byte[] bytes) {
 		return wrap(Arrays.copyOf(bytes, bytes.length));
 	}
 
+	/**
+	 * Returns {@code true} iff {@code o} is a byte vector with contents identical
+	 * to this byte vector.
+	 *
+	 * @param o  the object to test for equality with this byte vector
+	 * @return  {@code true} iff {@code o} is a byte vector with identical contents
+	 *          to this byte vector
+	 */
 	@Override
 	public boolean equals(Object o) {
 		if (this == o) return true;
@@ -49,14 +80,34 @@ public class ByteVector {
 		return Arrays.hashCode(bytes);
 	}
 
+	/**
+	 * Returns the length of the byte vector (i.e., the length of the underlying array).
+	 *
+	 * @return  the length of this byte vector
+	 */
 	public int size() {
 		return bytes.length;
 	}
 
+	/**
+	 * Returns the {@code index}-th element of the byte vector.
+	 *
+	 * @param index  the index, must be in the range between 0 (inclusive) and {@code size()}
+	 *               (exclusive)
+	 * @return  the {@code index}-th element of this byte vector
+	 *
+	 * @throws ArrayIndexOutOfBoundsException  if {@code index} is outside of the permitted
+	 * 	                                       range
+	 */
 	public byte get(int index) {
 		return bytes[index];
 	}
 
+	/**
+	 * Copy the contents of this byte vector to a freshly allocated new array.
+	 *
+	 * @return  a new array containing a copy of the contents of this byte vector
+	 */
 	public byte[] copyToNewArray() {
 		return Arrays.copyOf(bytes, bytes.length);
 	}
