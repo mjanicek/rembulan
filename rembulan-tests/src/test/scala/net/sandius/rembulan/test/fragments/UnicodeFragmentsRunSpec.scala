d28caed4bbd1c67752764caa1e42e66e142b47de
Miroslav Janíček <mira.janicek@gmail.com>
2016-10-10 21:31:51 +0200

    Adding tests for Unicode string handling.

---
+++ rembulan-tests/src/test/scala/net/sandius/rembulan/test/fragments/UnicodeFragmentsRunSpec.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2016 Miroslav Janíček
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.sandius.rembulan.test.fragments
+
+import net.sandius.rembulan.test.FragmentExecTestSuite
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class UnicodeFragmentsRunSpec extends FragmentExecTestSuite {
+
+  override def bundles = Seq(UnicodeFragments)
+  override def expectations = Seq(UnicodeFragments)
+  override def contexts = Seq(Full)
+
+  override def steps = Seq(1, Int.MaxValue)
+
+}
